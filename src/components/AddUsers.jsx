ca88629b9e9be9c0e69968f81432878f0d80481b
Dio Lemos <pedro.devsc@outlook.com>
2023-05-11 03:00:06 -0300

    update usersList component

--- src/components/AddUsers.jsx
+++ src/components/AddUsers.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-
+import styles from './Addusers.module.css'
 const AddUsers = (props)=>{
 
-    return (<div>
+    return (<div className={styles.container}>
+        <label htmlFor="name">User name</label>
+        <input type="text"  id="name"  />
+
+        <label htmlFor="age">User age</label>
+        <input type="number"  id="age" />
 
 
+        <button>Add user!</button>
     </div>)
 }
 
